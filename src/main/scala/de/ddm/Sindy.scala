068d178bbb7fadd506a17f0ff24df5b829e1b8bb
GruberSt3f4n <gstefan271@gmail.com>
2024-02-01 16:53:40 +0100

    implemented Algo

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -1,6 +1,7 @@
 package de.ddm
 
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import scala.collection.mutable.ListBuffer
 
 object Sindy {
 
@@ -15,7 +16,50 @@ object Sindy {
   }
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    //import implicit conversion to convert Scala objects to Spark Datasets
+    import spark.implicits._
 
-    // TODO
+    //read all data using the readData method and store it in a map
+    inputs.map(input => readData(input, spark))
+
+      //for each dataset, flatten dataset by row => (Column, Value)
+      .map(table => {
+        val columns = table.columns
+        table.flatMap(row => {
+          for (i <- columns.indices) yield {
+            (columns(i), row.getString(i))
+          }
+        })
+      })
+
+      //combine all datasets into one
+      .reduce(_ union _)
+
+      //group (Column, Value) by Column and just keep Value
+      .groupByKey(tuple => tuple._2)
+
+      //create set of the values for each key
+      .mapGroups { case (key, iter) => iter.map(_._1).toSet }
+
+      //pair each attribute with all other attributes in set (excluding itself) -> create all tasks
+      .flatMap(groups => groups
+        .map(currentKey =>
+          (currentKey, groups.filter(key => key != currentKey))))
+
+      //group pairs by key
+      .groupByKey(row => row._1)
+
+      //for each key produce set of keys that are common to it (set of INDs)
+      .mapGroups { case (key, iter) => (key, iter.map(_._2).reduce(_ intersect _))}
+
+      //filter where set of common keys is empty (where no INDs were found)
+      .filter(row => row._2.nonEmpty)
+
+      //collect results and sort by key
+      .collect()
+      .sortBy(ind => ind._1)
+
+      //print the results ()
+      .foreach(ind => println(ind._1 + " < " + ind._2.mkString(", ")))
   }
 }
